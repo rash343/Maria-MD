991a9a06f136234635af036e8bf26edec74ddf2e
rash343 <66876169+rash343@users.noreply.github.com>
2024-07-08 17:23:02 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,13 +3,11 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = [ process.env.OWNER_NUMBER || "+233558012648"],
+global.ownername = process.env.OWNER_NAME || "Raybrute(IRA)",
+global.location = "Ghana"
 
-global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
+global.botname = process.env.BOTNAME || "IRA",//name of the bot
 
 //sticker details
 global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
